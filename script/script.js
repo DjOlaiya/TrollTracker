e62b678f9bd1ddf4795baca0d77035b8f2b59438
Alan Yong <yonga@mymacewan.ca>
2017-01-28 15:48:24 -0700

    All of Trump's tweets are now poop. Now works with AJAX!

--- script/script.js
+++ script/script.js
@@ -1,19 +1,28 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Tweet Square Adder Script
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description
 // @author       You
 // @match        https://twitter.com/realdonaldtrump
 // @grant        none
+// @require https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    waitForKeyElements('p.tweet-text', filter);
+
     // Your code here...
-    var tweets = document.getElementsByClassName("tweet");
-    for (var i = 0; i < tweets.length; ++i) {
-        tweets[i].style.display = 'none';
-    };
+    function filter (tweet) {
+        console.log(tweet);
+        tweet.context.innerHTML = 'Poop';
+        var square = document.createElement('div');
+        square.style.width = '16px';
+        square.style.height = '16px';
+        square.style.background = 'red';
+        tweet.context.appendChild(square);
+    }
+
 })();
