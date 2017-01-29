6daf8023e09b142b31c18e446be7c973cd1d058f
JackStaples <staplesj3@mymacewan.ca>
2017-01-28 20:20:08 -0700

    server to open new page

--- server/app/server.js
+++ server/app/server.js
@@ -10,4 +10,9 @@ app.use(express.static(__dirname + '/../public' ));
 app.get('/', function(req, res){
   res.render((__dirname + '/../public/views/home.ejs'));
 });
+
+app.get('/results', function(req, res){
+  res.render((__dirname + '/../public/views/results.ejs'));
+});
+
 module.exports = app;
